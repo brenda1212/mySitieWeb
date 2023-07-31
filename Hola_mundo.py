f26b0b48e8563163ccae61100e54165d97af720e
sairaf3557 <sairafbrenda25@gmail.com>
2023-07-31 15:35:19 -0600

    SeleniumPython

--- Hola_mundo.py
+++ Hola_mundo.py
@@ -2,5 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
-driver.get("http://selenium.dev")
+driver.get("https://cinepolis.com/")
+
 driver.close()
+
