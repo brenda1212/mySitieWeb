90543c57e5a9ad2a897dac39146e774f505f71fc
sairaf3557 <sairafbrenda25@gmail.com>
2023-08-08 15:48:05 -0600

    SeleniumPython

--- Hola_mundo.py
+++ Hola_mundo.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
 driver.get("https://cinepolis.com/")
 
-driver.close()
+elemento_por_nombre = driver.find_element(By.NAME, "ctl00$cmbCiudades")
 
+print(elemento_por_nombre.text)
+
+
+driver.close()
