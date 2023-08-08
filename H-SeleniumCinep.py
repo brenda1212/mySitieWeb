b1902459f5ee0655e1768f4acd8227269a255ecd
sairaf3557 <sairafbrenda25@gmail.com>
2023-08-08 17:52:10 -0600

    SeleniumPython-Rename file

--- H-SeleniumCinep.py
+++ H-SeleniumCinep.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
 driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
 driver.get("https://cinepolis.com/")
+
 
 elemento_por_nombre = driver.find_element(By.NAME, "ctl00$cmbCiudades")
 
 print(elemento_por_nombre.text)
-
 
 driver.close()
