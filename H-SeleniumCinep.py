8b45ab122353f0e646ede494c1678b14cddee375
sairaf3557 <sairafbrenda25@gmail.com>
2023-08-08 15:57:16 -0600

    SeleniumPython-Rename file

--- H-SeleniumCinep.py
+++ H-SeleniumCinep.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
 driver.get("https://cinepolis.com/")
 
