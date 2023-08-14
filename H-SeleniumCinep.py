365f4ad15cb6b0fd864a34d99143143a54e40f36
sairaf3557 <sairafbrenda25@gmail.com>
2023-08-14 17:01:32 -0600

    droplist"Select"

--- H-SeleniumCinep.py
+++ H-SeleniumCinep.py
@@ -4,11 +4,8 @@ from selenium.webdriver.support.select import Select
 import time
 driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
 driver.get("https://cinepolis.com/")
-
-
 elementonombre=driver.find_element(By.NAME, 'ctl00$cmbCiudades')
 drop=Select(elementonombre)
-
 drop.select_by_value("30")
 time.sleep(4)
 
