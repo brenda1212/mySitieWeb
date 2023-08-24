b918ed80b970c8e37ecee6b86fa4b711cdd23fbc
sairaf3557 <sairafbrenda25@gmail.com>
2023-08-23 20:58:14 -0600

    SeleniumPython-Rename file

--- H-SeleniumCinep.py
+++ H-SeleniumCinep.py
@@ -1,10 +1,13 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 import time
-driver = webdriver.Chrome(executable_path="C:\\Users\\sairaf\\Downloads\\Chomedriver\\chromedriver.exe")
+
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options = option)
 driver.get("https://cinepolis.com/")
-elementonombre=driver.find_element(By.NAME, 'ctl00$cmbCiudades')
+elementonombre=driver.find_element(By.NAME,'ctl00$cmbCiudades')
 drop=Select(elementonombre)
 
 drop.select_by_visible_text("Chilpancingo")
@@ -12,10 +15,9 @@ drop.select_by_visible_text("Chilpancingo")
 nameplaces=driver.find_element(By.NAME,'ctl00$cmbComplejos')
 list=Select(nameplaces)
 list.select_by_visible_text("Cinépolis Galerías Chilpancingo")
+opselect=driver.find_element(By.ID,'cmbFechas')
+drlistt=Select(opselect)
+drlistt.select_by_visible_text("25 agosto")
 time.sleep(4)
-
-
 driver.close()
-
-
 
